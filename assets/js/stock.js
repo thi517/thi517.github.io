2e48ad6f9fd20497bc99c154078c92418ef6e005
Thi <thi517@gmail.com>
2018-02-13 18:18:15 -0500

    update url

--- assets/js/stock.js
+++ assets/js/stock.js
@@ -2,12 +2,12 @@ var stocks = ["VXX","$UST10Y","SPY", "EEM", "IYR", "USO", "UNG"];
 
 $(document).ready(function(){
     stocks.map(stock => {
-        var chartUrl = `http://stockcharts.com/c-sc/sc?s=${stock}&p=D&b=5&g=0&i=p12494722547&r=1518497514859`;
+        var chartUrl = `http://stockcharts.com/h-sc/ui?s=${stock}&p=D&b=5&g=0&id=p88631250574`;
         //console.log(chartUrl);
         $('#daily').append('<a href="' + chartUrl + '" class="btn btn-primary btn-lg btn-block" role="button" aria-disabled="true">' + stock + '</a>');
     });
     stocks.map(stock => {
-        var chartUrl = `http://stockcharts.com/c-sc/sc?s=${stock}&p=W&b=5&g=0&i=t57884206221&r=1518497601315`;
+        var chartUrl = `http://stockcharts.com/h-sc/ui?s=${stock}&p=W&b=5&g=0&id=p88631250574`;
         //console.log(chartUrl);
         $('#weekly').append('<a href="' + chartUrl + '" class="btn btn-primary btn-lg btn-block" role="button" aria-disabled="true">' + stock + '</a>');
     });
