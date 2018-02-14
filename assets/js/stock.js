ff49dcca3d869d4010d5a4ad6dec8b3d81aec57e
Thi <thi517@gmail.com>
2018-02-14 18:46:19 -0500

    update chart url

--- assets/js/stock.js
+++ assets/js/stock.js
@@ -1,13 +1,13 @@
-var stocks = ["VXX","$UST10Y","SPY", "EEM", "IYR", "USO", "UNG"];
+var stocks = ["VXX","$UST10Y","SPY", "EEM", "IYR", "USO", "UNG", "SOXX", "XLU"];
 
 $(document).ready(function(){
     stocks.map(stock => {
-        var chartUrl = `http://stockcharts.com/h-sc/ui?s=${stock}&p=D&b=5&g=0&id=p88631250574`;
+        var chartUrl = `http://stockcharts.com/h-sc/ui?s=${stock}&p=D&b=5&g=0&id=p54396728978`;
         //console.log(chartUrl);
         $('#daily').append('<a href="' + chartUrl + '" class="btn btn-primary btn-lg btn-block" role="button" aria-disabled="true">' + stock + '</a>');
     });
     stocks.map(stock => {
-        var chartUrl = `http://stockcharts.com/h-sc/ui?s=${stock}&p=W&b=5&g=0&id=p88631250574`;
+        var chartUrl = `http://stockcharts.com/h-sc/ui?s=${stock}&p=W&b=5&g=0&id=p54396728978`;
         //console.log(chartUrl);
         $('#weekly').append('<a href="' + chartUrl + '" class="btn btn-primary btn-lg btn-block" role="button" aria-disabled="true">' + stock + '</a>');
     });
